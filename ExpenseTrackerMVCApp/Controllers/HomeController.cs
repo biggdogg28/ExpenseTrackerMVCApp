4b7f26feaf1b2593f674a61b794e5dd43bcbb6bc
Lehel Veibel <lehel.veibel@umtsoftware.com>
2023-04-07 22:34:28 +0300

    Added Total Chart on Home page

--- ExpenseTrackerMVCApp/Controllers/HomeController.cs
+++ ExpenseTrackerMVCApp/Controllers/HomeController.cs
@@ -1,7 +1,15 @@
-﻿using ExpenseTrackerMVCApp.Models;
+﻿using ASPNET_MVC_Samples.Models;
+using ExpenseTrackerMVCApp.Models;
 using ExpenseTrackerMVCApp.Repositories;
 using Microsoft.AspNetCore.Mvc;
+using Newtonsoft.Json;
 using System.Diagnostics;
+using System;
+using System.Collections.Generic;
+using System.Dynamic;
+using System.Linq;
+using System.Web;
+using System.Runtime.Serialization;
 
 namespace ExpenseTrackerMVCApp.Controllers
 {
@@ -9,17 +17,35 @@ namespace ExpenseTrackerMVCApp.Controllers
     {
         private readonly ILogger<HomeController> _logger;
         private readonly TotalsRepository _totalsRepository;
+        private readonly IncomeRepository _incomeRepository;
+        private readonly ExpensesRepository _expensesRepository;
 
-        public HomeController(ILogger<HomeController> logger, TotalsRepository totalsRepository)
+        public HomeController(ILogger<HomeController> logger, TotalsRepository totalsRepository, IncomeRepository incomeRepository, ExpensesRepository expensesRepository)
         {
             _logger = logger;
             _totalsRepository = totalsRepository;
+            _incomeRepository = incomeRepository;
+            _expensesRepository = expensesRepository;
         }
 
         public IActionResult Index()
         {
+            var incomes = _incomeRepository.GetIncomes();
+            decimal sumI = incomes.Sum(x => x.Amount);
+            var expenses = _expensesRepository.GetExpenses();
+            decimal sumE = expenses.Sum(x => x.Amount);
+            decimal balance = sumI - sumE;
+
+            List<DataPoint> dataPoints = new List<DataPoint>{
+                new DataPoint(10, sumI),
+                new DataPoint(20, sumE)
+            };
+
+            ViewBag.DataPoints = JsonConvert.SerializeObject(dataPoints);
+
             var totals = _totalsRepository.GetTotal();
             return View("Index", totals);
+
         }
 
         public IActionResult Privacy()
